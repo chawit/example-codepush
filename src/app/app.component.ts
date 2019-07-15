9d1cdc0b245915f78fc7ce475f6280ae16c7c313
CHAWIT WIRIYAKUN <chawitmild@gmail.com>
2019-07-15 15:31:08 +0700

    finish project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+declare let codePush,InstallMode: any; 
+document.addEventListener("resume", function () {
+  //codePush.sync();
+  codePush.sync(null, { updateDialog: true, installMode: InstallMode.IMMEDIATE });
+});
+window.addEventListener("load", function () {
+  //codePush.sync();
+  codePush.sync(null, { updateDialog: true, installMode: InstallMode.IMMEDIATE });
+});
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
